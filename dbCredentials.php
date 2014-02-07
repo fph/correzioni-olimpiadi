34010725467125bc9ce5b6ede399d89db2136f91
Giada Franz <walypala23@gmail.com>
2014-02-07 23:38:32 +0100

    fix

--- dbCredentials.php
+++ dbCredentials.php
@@ -6,6 +6,7 @@ $dbName='CorOli';
 
 function escape_input($value)
 {
+	if (is_null($value)) $value="";
 	if (get_magic_quotes_gpc()) $value = stripslashes($value);
 	if (!is_numeric($value)) $value = "'" . mysql_real_escape_string($value) . "'";
 	return $value;
