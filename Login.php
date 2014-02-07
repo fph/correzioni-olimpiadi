d47678670145cc56c0e0f7d5f5bfcb2aedd39d75
Federico Glaudo <dario2994@gmail.com>
2014-02-08 00:21:07 +0100

    Fix

--- Login.php
+++ Login.php
@@ -1,4 +1,5 @@
 <?php
+
 include "dbCredentials.php";
 session_start();
 if (!isset($_SESSION["UserId"]) or time()-$_SESSION["LoginTimestamp"]>3600) {
@@ -6,27 +7,25 @@ if (!isset($_SESSION["UserId"]) or time()-$_SESSION["LoginTimestamp"]>3600) {
 	session_destroy();
 }
 else {
-	header("index.php");
+	header("Location: index.php");
 }
 
 if (!is_null($_POST["user"])) {
 	$user=escape_input($_POST["user"]);
-	echo password_hash('acca',PASSWORD_DEFAULT);
-	$psw=escape_input(password_hash($_POST["psw"],PASSWORD_DEFAULT));
-	echo 1;
+	$psw=escape_input(passwordHash($_POST["psw"]));
 	$db=new mysqli($dbServer, $dbUser, $dbPass);
 	if ($db->connect_errno) die($db->connect_error);
 	
 	$db->select_db($dbName);
 
-	$query="SELECT 1 id FROM Users WHERE user=$user AND passHash=$psw";
+	$query="SELECT id FROM Users WHERE user=$user AND passHash=$psw";
 	$result=$db->query($query) or die($db->error);
-
+	
 	if ($UserId=mysqli_fetch_array($result)['id']){
 		session_start();
 		$_SESSION["UserId"]=$UserId;
-		$_SESSION["timestamp"]=time();
-		header("index.php");
+		$_SESSION["LoginTimestamp"]=time();
+		header("Location: index.php");
 	}
 	else {
 		echo "Username o password errati.\n";
