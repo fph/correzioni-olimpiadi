4020ab57adbac9e5c971dedbdfd5c2f149259542
Giada Franz <walypala23@gmail.com>
2014-02-07 23:38:05 +0100

    Aggiunta prima versione del Login

---
+++ Login.php
@@ -0,0 +1,41 @@
+<?php
+
+include "dbCredentials.php";
+session_start();
+if (!isset($_SESSION["UserId"]) or time()-$_SESSION["LoginTimestamp"]>3600) {
+	session_unset();
+	session_destroy();
+}
+else {
+	header("index.php");
+}
+
+if (!is_null($_POST["user"])) {
+	$user=escape_input($_POST["user"]);
+	$psw=escape_input(password_hash($_POST["psw"],PASSWORD_DEFAULT));
+	$db=new mysqli($dbServer, $dbUser, $dbPass);
+	if ($db->connect_errno) die($db->connect_error);
+
+	$db->select_db($dbName);
+
+	$query="SELECT 1 id FROM Users WHERE user=$user AND passHash=$psw";
+	$result=$db->query($query) or die($db->error);
+
+	if ($UserId=mysqli_fetch_array($result)['id']){
+		session_start();
+		$_SESSION["UserId"]=$UserId;
+		$_SESSION["timestamp"]=time();
+		header("index.php");
+	}
+	else {
+		echo "Username o password errati.\n";
+	}
+}
+
+?>
+
+<form name=input" method="POST" action="Login.php">
+	<input type="text" id="user" value="Username">
+	<input type="text" id="psw" value="Password">
+	<input type="submit" value="Login">
+</form>
