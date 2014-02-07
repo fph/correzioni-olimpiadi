1c161f2230d7679a284ee3f14f9ff105349fc1cb
Federico Glaudo <dario2994@gmail.com>
2014-02-08 00:21:51 +0100

    Spostato le funzioni in un file a parte da dbCredentials

---
+++ sqlUtilities.php
@@ -0,0 +1,14 @@
+<?php
+function escape_input($value)
+{
+	if (is_null($value)) $value="";
+	if (get_magic_quotes_gpc()) $value = stripslashes($value);
+	if (!is_numeric($value)) $value = "'" . mysql_real_escape_string($value) . "'";
+	return $value;
+}
+
+function passwordHash($pass) {
+	$salt="trecentoquarantaseidue";
+	return password_hash($pass,PASSWORD_DEFAULT,['salt'=>$salt]);
+}
+?>
