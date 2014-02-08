3c55a4dd16ef7c36cc7ebe9d4460826a387229a9
Federico Glaudo <dario2994@gmail.com>
2014-02-08 18:20:23 +0100

    Scritta la barra alta

--- View/UpperBar.php
+++ View/UpperBar.php
@@ -1,3 +1,10 @@
-<div id="upperBar">
-	<a href="" />
-</div>
+<head>
+  <link href="global.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<div id="upperBar">
+		<div id="innerBar">
+			<a href="Logout.php" id="logout"> Logout </a>
+		</div>
+	</div>
+</body>
