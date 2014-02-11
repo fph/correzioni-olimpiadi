4dee706ed3b8c722502af96184de9552d5e53e5a
Federico Glaudo <dario2994@gmail.com>
2014-02-11 22:24:03 +0100

    Cambiato l'interazione col database

--- AskInformation.php
+++ AskInformation.php
@@ -2,9 +2,56 @@
 	require_once "Utilities.php";
 	SuperRequire_once("General", "sqlUtilities.php");
 	
-	//~ function RequestFromTable($tableName, $info, $data) {
-		//~ $query="SELECT ".$info." FROM ".dbName.".".$tableName." WHERE ";
-	//~ }
+	function OpenDbConnection() {
+		$db=new mysqli(dbServer, dbUser, dbPass);
+		if ($db->connect_errno) die($db->connect_error);
+		
+		$db->select_db(dbName);
+		return $db;
+	}
+	
+	function QuerySelect($tableName, $data=NULL, $info=NULL) {
+		$query="SELECT ";
+		if( !is_null($info) ) {
+			$first=0;
+			foreach ( $info as $x ) {
+				if( $first==0 ){
+					$query .=$x.' '; 
+					$first=1;
+				}
+				else $query .=', '.$x.' ';
+			}
+		}
+		else $query.='* ';
+		$query.=" FROM ".dbName.".".$tableName.' ';
+		
+		if( !is_null( $data ) ) {
+			$query .='WHERE ';
+			$first=0;
+			foreach ( $data as $field => $value ) {
+				if( $first==0 ) {
+					$query .= $field.'='.escape_input($value).' ';
+					$first=1;
+				}
+				else $query .= 'AND '.$field.'='.escape_input($value).' ';
+			}
+		} 
+		
+		return $query;
+	}
+	
+	function OneResultQuery($db, $query) {
+		$result=$db->query($query) or die($db->error);
+		return mysqli_fetch_array($result);
+	}
+	
+	function ManyResultQuery($db, $query) {
+		$result=$db->query($query) or die($db->error);
+		$arr=[];
+		$n=0;
+		while($arr[$n]=mysqli_fetch_array($result))$n++;
+		return $arr;
+	}
 	
 	function AskWholeTable($table){
 		global $TableInformation;
--- Model/Login.php
+++ Model/Login.php
@@ -2,35 +2,37 @@
 require_once "../Utilities.php";
 
 SuperRequire_once("General", "SessionManager.php");
-echo 1;
 SuperRequire_once("General", "AskInformation.php");
-echo 2;
 SuperRequire_once("General", "TemplateCreation.php");
 
-$SessionStatus=CheckSession();
+$db=OpenDbConnection();
+echo OneResultQuery($db, QuerySelect('Contestants',['id'=>2],['name','surname']) )['name'];
+$db->close();
 
-if( CheckSession() == -1 ) {
-	EndSession();
-	$v_ErrorMessage="Your session is expired, login again.";
-	TemplatePage("Login","Error",0);
-	die();
-}
-else if( CheckSession() == 1 ) {
-	SuperRedirect("Model","index.php");
-	die();
-}
-else if ( CheckSession()==0 and !is_null($_POST["user"]) ) {
-	$UserId=VerifyCredentials($_POST["user"],$_POST["psw"]);
-	
-	if( $UserId == -1 ) {
-		$v_ErrorMessage="Incorrect username or password";
-		TemplatePage("Login","Error",0);
-	}
-	else {
-		StartSession($UserId,$_POST['user']);
-		SuperRedirect("Model","index.php");
-		die();
-	}
-}
-else if( CheckSession()==0 ) TemplatePage("Login","",0);
+//~ $SessionStatus=CheckSession();
+//~ 
+//~ if( CheckSession() == -1 ) {
+	//~ EndSession();
+	//~ $v_ErrorMessage="Your session is expired, login again.";
+	//~ TemplatePage("Login","Error",0);
+	//~ die();
+//~ }
+//~ else if( CheckSession() == 1 ) {
+	//~ SuperRedirect("Model","index.php");
+	//~ die();
+//~ }
+//~ else if ( CheckSession()==0 and !is_null($_POST["user"]) ) {
+	//~ $UserId=VerifyCredentials($_POST["user"],$_POST["psw"]);
+	//~ 
+	//~ if( $UserId == -1 ) {
+		//~ $v_ErrorMessage="Incorrect username or password";
+		//~ TemplatePage("Login","Error",0);
+	//~ }
+	//~ else {
+		//~ StartSession($UserId,$_POST['user']);
+		//~ SuperRedirect("Model","index.php");
+		//~ die();
+	//~ }
+//~ }
+//~ else if( CheckSession()==0 ) TemplatePage("Login","",0);
 ?>
