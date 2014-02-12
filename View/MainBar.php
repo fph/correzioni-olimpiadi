0d8ca1f640241027983dd26ab2e7d2e93320cb88
Federico Glaudo <dario2994@gmail.com>
2014-02-12 16:56:53 +0100

    Migliorato la barra in alto

--- View/MainBar.php
+++ View/MainBar.php
@@ -1,15 +1,19 @@
 <div id="MainBar_container">
 	<div id="MainBar_floater">
 		<div id="MainBar_inner">
-			<a href="AccountSettings.php" id="UsernameLink"> 
-				<span id="UsernameInner">
-					<?php
-						session_start();
-						echo $_SESSION['username'];
-					?>
-				</span>
-			</a>
-			<a href="Logout.php" id="logout"> Logout </a>
+			<h1 id="MainBar_title"> Olimpiadi di Matematica - Correzioni</h1>
+			
+			<div id="MainBar_UserList">
+				<a href="AccountSettings.php" id="UsernameLink"> 
+					<span id="UsernameInner">
+						<?php
+							session_start();
+							echo $_SESSION['username'];
+						?>
+					</span>
+				</a>
+				<a href="Logout.php" id="logout"> Logout </a>
+			</div>
 		</div>
 	</div>
 </div>
