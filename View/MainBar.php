bee3886d73fe63838d895de7d830d397be81971f
Federico Glaudo <dario2994@gmail.com>
2014-02-12 17:05:20 +0100

    Fixato il bug del logout che non scompare

--- View/MainBar.php
+++ View/MainBar.php
@@ -3,17 +3,21 @@
 		<div id="MainBar_inner">
 			<h1 id="MainBar_title"> Olimpiadi di Matematica - Correzioni</h1>
 			
+			<?php
+				session_start();
+				if( isset($_SESSION['UserId']) ) {
+			?>
 			<div id="MainBar_UserList">
 				<a href="AccountSettings.php" id="UsernameLink"> 
 					<span id="UsernameInner">
 						<?php
-							session_start();
 							echo $_SESSION['username'];
 						?>
 					</span>
 				</a>
 				<a href="Logout.php" id="logout"> Logout </a>
 			</div>
+			<?php } ?>
 		</div>
 	</div>
 </div>
