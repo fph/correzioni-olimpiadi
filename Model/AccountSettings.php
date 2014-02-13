21ccd17f48bf85a3ec1afe546cc3e1ed0a017bfe
Federico Glaudo <dario2994@gmail.com>
2014-02-13 22:16:58 +0100

    Completato la configurazione dell'account personale

--- Model/AccountSettings.php
+++ Model/AccountSettings.php
@@ -6,18 +6,58 @@ SuperRequire_once("General","AskInformation.php");
 
 if( !is_null( $_POST["NewUsername"] ) ) {
 	$db=OpenDbConnection();
+	
+	$SameUsername=OneResultQuery($db, QuerySelect('Users',['username'=>$_POST['NewUsername']]));
+	
+	if( !is_null( $SameUsername ) ) {
+		$db->close();
+		TemplatePage("AccountSettings",
+		['Index'=>'index.php','Configurazione Account'=>'AccountSettings.php'],1,
+		['type'=>'bad', 'text'=>'Username già in uso da un utente, scegline un altro.']);
+		die();
+	}
+	
 	Query($db, QueryUpdate('Users',['id'=>GetUserIdBySession()],['username'=>$_POST['NewUsername']]));
 	
+	$db->close();
 	StartSession(GetUserIdBySession(),$_POST['NewUsername']);
 	
-	TemplatePage("AccountSettings",['Index'=>'index.php','Configurazione Account'=>'AccountSettings.php'],1,['type'=>'good', 'text'=>'Username changed successfully.']);
+	TemplatePage("AccountSettings",
+	['Index'=>'index.php','Configurazione Account'=>'AccountSettings.php'],1,
+	['type'=>'good', 'text'=>'Username cambiato con successo.']);
 	
 	die();
 }
 
-else if ( 0 ) {
+else if ( !is_null( $_POST['NewPassword'] ) and !is_null( $_POST['OldPassword'] ) ) {
+	
+	if( strlen( $_POST['NewPassword'] ) < 4 ) {
+		TemplatePage("AccountSettings",
+		['Index'=>'index.php','Configurazione Account'=>'AccountSettings.php'],1,
+		['type'=>'bad', 'text' => 'La nuova password è troppo breve.']);
+	}
+	$db=OpenDbConnection();
+	
+	$CorrectOldPass=OneResultQuery($db,QuerySelect('Users',['id'=>GetUserIdBySession(),'passHash'=>passwordHash($_POST['OldPassword']) ] ) );
+	
+	if( is_null($CorrectOldPass) ) {
+		$db->close();
+		TemplatePage("AccountSettings",
+		['Index'=>'index.php','Configurazione Account'=>'AccountSettings.php'],1,
+		['type'=>'bad', 'text'=>'La vecchia password non è corretta.']);
+		
+		die();
+	}
+	
+	Query($db,QueryUpdate('Users',['id'=>GetUserIdBySession()],['passHash'=>passwordHash($_POST['NewPassword'])]));
+	
+	$db->close();
+	TemplatePage("AccountSettings",
+	['Index'=>'index.php','Configurazione Account'=>'AccountSettings.php'],1,
+	['type'=>'good', 'text'=>'Password cambiata con successo.']);
 	die();
 }
 
 TemplatePage("AccountSettings",['Index'=>'index.php','Configurazione Account'=>'AccountSettings.php']);
+die();
 ?>
--- View/AccountSettings.php
+++ View/AccountSettings.php
@@ -4,3 +4,8 @@
 <input type="text" name="NewUsername" value="NuovoUsername"> 
 <input type="submit" value="Salva"> 
 </form>
+<form action="" method="POST" name="PasswordChange">
+<input type="text" name="OldPassword" value="Vecchia Password">
+<input type="text" name="NewPassword" value="Nuova Password"> 
+<input type="submit" value="Salva"> 
+</form>
