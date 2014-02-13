0518c311772deb2815c9052828f55707c06d1934
Federico Glaudo <dario2994@gmail.com>
2014-02-13 21:33:58 +0100

    Fatto la gestione ajax lato client

--- View/AjaxManager.js
+++ View/AjaxManager.js
@@ -1,22 +1,19 @@
-function ManageAjaxRequest() {
-	if ( ajaxReq.readyState==4 && ajaxReq.status==200) {
-		var text=ajaxReq.responseText; //Magari usare json!
-	}
+function showMessage( type , text ) {
+	alert(type+': '+text);
 }
 
-function MakeAjaxRequest( $url, $data ) {
+function MakeAjaxRequest( url, data ) {
 	var ajaxReq;
 	if (window.XMLHttpRequest)ajaxReq = new XMLHttpRequest();
 	else ajaxReq = new ActiveXObject("Microsoft.XMLHTTP");
 	ajaxReq.open("POST", url, true);
 	ajaxReq.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-	var PostData='';
-	var first=0;
-	for ( var key in $data ) {
-		if ( first==1 ) PostData+='&';
-		PostData+=key+'='+data[key]; //da fare l'escape!
-		first=1;
-	}
-	ajaxReq.send( PostData );
-	ajaxReq.onreadystatechange = showMessage;
+	
+	ajaxReq.send( 'data='+JSON.stringify( data ) );
+	ajaxReq.onreadystatechange = function() {
+		if ( ajaxReq.readyState==4 && ajaxReq.status==200) {
+			var msg=JSON.parse( ajaxReq.responseText ); //Magari usare json!
+			showMessage(msg.type, msg.text);
+		}
+	};
 }
