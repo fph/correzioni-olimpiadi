922519fb31a2f142224c7227fba6a2e7b0eff01c
Federico Glaudo <dario2994@gmail.com>
2014-02-13 11:32:49 +0100

    Prima versione dei css del PagePath

--- View/PagePath.php
+++ View/PagePath.php
@@ -5,13 +5,13 @@
 		foreach($v_PathDescription as $name => $link) {
 			if( $first==1 ) { 
 	?>
-				<div class='PathSeparator'> < </div>
+				<strong class='PathSeparator'> < </strong>
 	<?php } ?>
-			<div class='PathElement'> <a href='<?php echo $link ?>'>
+			<a class='PathElement' href='<?php echo $link ?>'>
 	<?php
 			echo $name;
 			$first=1;
 	?> 
-			</a></div>
+			</a>
 	<?php } ?>
 </div>
