f833dd1c331a0c668e959b87e329bdb02bf10f38
Federico Glaudo <dario2994@gmail.com>
2014-02-14 19:19:40 +0100

    Fixato i css

--- View/AccountSettings.php
+++ View/AccountSettings.php
@@ -1,39 +1,44 @@
-<h1 class='PageTitle'> Configurazione account</h1>
-<h2 class='PageSubTitle'> <?php session_start(); echo $_SESSION['username']; ?> </h2>
-<form action="AccountSettings.php" method="POST" id="UsernameChange">
-	<table>
-	<tr>
-		<th> Nuovo username </th>
-		<th> </th>
-	</tr>
-	<tr>
-		<td>
-			<input type="text" name="NewUsername"> 
-		</td>
-		<td>
-			<input type="submit" value="Salva"> 
-		</td>
-	</tr>
-	</table>
-</form>
-
-<form action="AccountSettings.php" method="POST" name="PasswordChange">
-	<table>
+<h2 class='pageTitle'> Configurazione account</h2>
+<div id='UsernameChange_container' class='formContainer'>
+	<h3 class='pageSubtitle'> Cambio Username </h3>
+	<form action="AccountSettings.php" method="POST" id="UsernameChange">
+		<table>
 		<tr>
-			<th> Vecchia Password </th>
-			<th> Nuova Password</th>
+			<th> Nuovo username </th>
 			<th> </th>
 		</tr>
 		<tr>
 			<td>
-				<input type="password" name="OldPassword" >
-			</td>
-			<td>
-				<input type="password" name="NewPassword" > 
+				<input type="text" name="NewUsername"> 
 			</td>
 			<td>
 				<input type="submit" value="Salva"> 
 			</td>
 		</tr>
-	</table>
-</form>
+		</table>
+	</form>
+</div>
+
+<div id='PasswordChange_container' class='formContainer'>
+	<h3 class='pageSubtitle'> Cambio Password </h3>
+	<form action="AccountSettings.php" method="POST" name="PasswordChange">
+		<table>
+			<tr>
+				<th> Vecchia Password </th>
+				<th> Nuova Password</th>
+				<th> </th>
+			</tr>
+			<tr>
+				<td>
+					<input type="password" name="OldPassword" >
+				</td>
+				<td>
+					<input type="password" name="NewPassword" > 
+				</td>
+				<td>
+					<input type="submit" value="Salva"> 
+				</td>
+			</tr>
+		</table>
+	</form>
+</div>
--- View/Login.php
+++ View/Login.php
@@ -1,15 +1,17 @@
 <h2 class='pageTitle'> Login </h2>
-<form class='UserForm' id='LoginForm' method="POST" action="Login.php">
-	<table>
-	<tr>
-		<th> Username</th>
-		<th> Password</th>
-		<th> </th>
-	</tr>
-	<tr>
-		<td> <input type="text" name="username"> </td>
-		<td> <input type="password" name="password"> </td>
-		<td> <input type="submit" value="Login"> </td>
-	</tr>
-	</table>
-</form>
+<div class='formContainer'>
+	<form class='UserForm' id='LoginForm' method="POST" action="Login.php">
+		<table>
+		<tr>
+			<th> Username</th>
+			<th> Password</th>
+			<th> </th>
+		</tr>
+		<tr>
+			<td> <input type="text" name="username"> </td>
+			<td> <input type="password" name="password"> </td>
+			<td> <input type="submit" value="Login"> </td>
+		</tr>
+		</table>
+	</form>
+</div>
