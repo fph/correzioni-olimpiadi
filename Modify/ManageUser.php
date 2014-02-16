5e395faf3a93951329350af9db27ffab85cfbcce
Federico Glaudo <dario2994@gmail.com>
2014-02-16 12:37:26 +0100

    Scritto ManageUser e debuggato

--- Modify/ManageUser.php
+++ Modify/ManageUser.php
@@ -4,48 +4,6 @@ SuperRequire_once('General','SessionManager.php');
 SuperRequire_once('General','sqlUtilities.php');
 SuperRequire_once('General','PermissionManager.php');
 
-$db=OpenDbConnection();
-
-$data=json_decode( $_POST['data'] , 1);
-$username=$data['username'];
-$password=$data['password'];
-
-$Permission=IsAdmin( $db, GetUserIdBySession() );
-
-if( $Permission==0 ) {
-	$db->close();
-	echo json_encode( ['type'=>'bad', 'text'=>'Non hai i permessi per aggiungere un correttore'] );
-	die();
-}
-
-//TODO: Controllare che lo username sia valido?
-
-$UsernameDuplicate=OneResultQuery($db,QuerySelect('Users',['username'==$username]));
-
-if( !is_null( $UsernameDuplicate ) ) {
-	$db->close();
-	echo json_encode( ['type'=>'bad', 'text'=>'È già presente un correttore con lo stesso username'] );
-	die();
-}
-
-if( strlen( $password ) < 4 ) {
-	$db->close();
-	echo json_encode( ['type'=>'bad', 'text'=>'La password deve avere almeno 4 caratteri'] );
-	die();
-}
-
-Query( $db,QueryInsert('Users', ['username'=>$username,'passHash'=>passwordHash($password) ]) );
-
-$db->close();
-echo json_encode( ['type'=>'good', 'text'=>'Correttore creato con successo'] );
-?>
-
-<?php
-require_once '../Utilities.php';
-SuperRequire_once('General','SessionManager.php');
-SuperRequire_once('General','sqlUtilities.php');
-SuperRequire_once('General','PermissionManager.php');
-
 //BUG: accenti e apostrofi non sono accettati (magari virgole per cognomi assurdi)
 function AddUser( $db , $username, $password ){
 	if( !is_string($username) or !is_string($password) ) {
@@ -53,7 +11,7 @@ function AddUser( $db , $username, $password ){
 	}
 	
 	
-	$UsernameDuplicate=OneResultQuery($db,QuerySelect('Users',['username'==$username]));
+	$UsernameDuplicate=OneResultQuery($db,QuerySelect('Users',['username'=>$username]));
 	if( !is_null( $UsernameDuplicate ) ) {
 		$db->close();
 		echo json_encode( ['type'=>'bad', 'text'=>'È già presente un correttore con lo stesso username'] );
@@ -101,7 +59,7 @@ function AddPermission( $db , $UserId , $ContestId ) {
 		return ['type'=>'bad' ,'text'=>'La gara selezionata non esiste'];
 	}
 	
-	$Permission=VerifyPermission($db,$Userid,$ContestId);
+	$Permission=VerifyPermission($db,$UserId,$ContestId);
 	if( $Permission==1 ) {
 		return ['type'=>'bad' ,'text'=>'Il permesso scelto è già presente'];
 	}
@@ -112,6 +70,10 @@ function AddPermission( $db , $UserId , $ContestId ) {
 }
 
 function RemovePermission( $db , $UserId , $ContestId ) {
+	if( IsAdmin($db, $UserId, $ContestId) == 1 ) {
+		return ['type'=>'bad', 'text'=>'Non puoi togliere permessi ad un admin'];
+	}
+	
 	$Exist1=OneResultQuery($db,QuerySelect('Permissions',['ContestId'=>$ContestId, 'UserId'=>$UserId]));
 	if( is_null($Exist1) ){
 		return ['type'=>'bad' ,'text'=>'Il permesso selezionato non esiste'];
