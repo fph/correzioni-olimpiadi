ed43cec3d0f3132c37d5336abb825f3b99c73fdd
Federico Glaudo <dario2994@gmail.com>
2014-02-16 12:39:10 +0100

    Riscritto InitDatabase sfruttando le funzioni di sqlUtilities

--- InitDatabase.php
+++ InitDatabase.php
@@ -1,20 +1,22 @@
 <?php
 require_once "Utilities.php";
 SuperRequire_once("General","sqlUtilities.php");
+SuperRequire_once("General","PermissionManager.php");
 
 function CreateDatabase() {
 	$db=new mysqli (dbServer, dbUser, dbPass);
 	if($db->connect_errno) die ($db->connect_error);
 	
-
 	$query='DROP DATABASE IF EXISTS `'.dbName.'`;'; //DEBUG
-	$db->query($query) or die($db->error);
+	Query($db, $query);
 
 	$query='CREATE DATABASE IF NOT EXISTS `'.dbName.'`;';
-	$db->query($query) or die($db->error);
+	Query($db, $query);
 
 	echo "Database ".dbName." created.\n";
-	$db->select_db(dbName) or die($db->error);
+	$db->close();
+	
+	$db=OpenDbConnection();
 	
 	$query=
 	'CREATE TABLE IF NOT EXISTS `Users` (
@@ -24,8 +26,7 @@ function CreateDatabase() {
 		PRIMARY KEY (`id`),
 		UNIQUE KEY(`username`)
 	) ENGINE=InnoDB;';
-	$db->query($query) or die($db->error);
-	
+	Query($db, $query);
 	echo "Table Users created.\n";
 	
 	$query=
@@ -36,8 +37,7 @@ function CreateDatabase() {
 		FOREIGN KEY (`UserId`) REFERENCES Users(`id`)
 			ON DELETE CASCADE ON UPDATE CASCADE
 	) ENGINE=InnoDB;';
-	$db->query($query) or die($db->error);
-	
+	Query($db, $query);
 	echo "Table Administrators created.\n";
 	
 	$query=
@@ -48,8 +48,7 @@ function CreateDatabase() {
 		PRIMARY KEY (`id`),
 		UNIQUE KEY(`name`)
 	) ENGINE=InnoDB;';
-	$db->query($query) or die($db->error);
-
+	Query($db, $query);
 	echo "Table Contests created.\n";
 
 	$query=
@@ -67,8 +66,7 @@ function CreateDatabase() {
 		FOREIGN KEY (`ContestId`) REFERENCES Contests(`id`)
 			ON DELETE CASCADE ON UPDATE CASCADE
 	) ENGINE=InnoDB;';
-	$db->query($query) or die($db->error);
-	
+	Query($db, $query);
 	echo "Table Permissions created.\n";
 	
 	$query=
@@ -79,8 +77,7 @@ function CreateDatabase() {
 		PRIMARY KEY (`id`),
 		UNIQUE KEY(`surname`)
 	) ENGINE=InnoDB;';
-	$db->query($query) or die($db->error);
-
+	Query($db, $query);
 	echo "Table Contestants created.\n";
 
 	$query=
@@ -98,8 +95,7 @@ function CreateDatabase() {
 		FOREIGN KEY (`ContestantId`) REFERENCES Contestants(`id`)
 			ON DELETE CASCADE ON UPDATE CASCADE
 	) ENGINE=InnoDB;';
-	$db->query($query) or die($db->error);
-
+	Query($db, $query);
 	echo "Table Participations created.\n";
 
 	$query=
@@ -114,8 +110,7 @@ function CreateDatabase() {
 		FOREIGN KEY (`ContestId`) REFERENCES Contests(`id`)
 			ON DELETE CASCADE ON UPDATE CASCADE
 	) ENGINE=InnoDB;';
-	$db->query($query) or die($db->error);
-
+	Query($db, $query);
 	echo "Table Problems created.\n";
 
 	$query=
@@ -138,241 +133,172 @@ function CreateDatabase() {
 		FOREIGN KEY (`UserId`) REFERENCES Users(`id`)
 			ON DELETE SET NULL ON UPDATE CASCADE
 	) ENGINE=InnoDB;';
-	$db->query($query) or die($db->error);
-
+	Query($db, $query);
 	echo "Table Corrections created.\n";
 
 	echo "All tables have been created.\n\n";
 	$db->close();
 }
 
-function PopulateUsers() {
-	$db=new mysqli (dbServer, dbUser, dbPass);
-	if($db->connect_errno) die ($db->connect_error);
-	
-	//~ echo "CIAO";
-	
-	$query="INSERT INTO ".dbName.".`Users` (`username`,`passHash`) VALUES
-		('Xamog','".passwordHash('meraviglioso')."'),
-		('LudoP','".passwordHash('yochicco')."'),
-		('dario2994','".passwordHash('acca')."'),
-		('fph','".passwordHash('pizzica')."'),
-		('walypala23','".passwordHash('gamma')."'),
-		('SimoTheWolf','".passwordHash('vero o falso?')."');";
-	//~ echo $query;
-	$db->query($query) or die($db->error);
-	$db->close();
+function PopulateUsers($db) {
+	$Users=['Xamog'=>'meraviglioso', 
+			'LudoP'=>'yochicco', 
+			'dario2994'=>'acca', 
+			'fph'=>'pizzica', 
+			'walypala23'=>'gamma', 
+			'SimoTheWolf'=>'vero o falso?'];
+	foreach($Users as $username => $password){
+		Query($db,QueryInsert('Users',['username'=>$username, 'passHash'=>passwordHash($password)]));
+	}
 
 	echo "Table Users Populated.\n";
 }
 
-function PopulateAdministrators() {
-	$db=new mysqli (dbServer, dbUser, dbPass);
-	if($db->connect_errno) die ($db->connect_error);
-	
-	$query="SELECT id FROM ".dbName.".Users WHERE username='dario2994';";
-	//~ echo $query;
-	$dario2994_id=$db->query($query) or die($db->error);
-	$query="INSERT INTO ".dbName.".Administrators (UserId) VALUES (".mysqli_fetch_array($dario2994_id)['id'].");";
-	$db->query($query) or die($db->error);
-	$db->close();
+function PopulateAdministrators($db) {
+	$dario2994_id=OneResultQuery($db,QuerySelect('Users',['username'=>'dario2994'],['id']))['id'];
+	Query($db,QueryInsert('Administrators',['UserId'=>$dario2994_id]));
 
 	echo "Table Users Populated.\n";
 }
 
-function PopulateContests() {
-	$db=new mysqli (dbServer, dbUser, dbPass);
-	if($db->connect_errno) die ($db->connect_error);
-	
-	$query="INSERT INTO ".dbName.".`Contests` (`name`,`date`) VALUES
-		('Senior 2013','1994-04-29'),
-		('WinterCamp 2011 Ammissione','1994-04-29'),
-		('Preimo 2010 TST giorno 1','2013-12-25'),
-		('Preimo 2010 TST giorno 2','2010-08-15'),
-		('IMO 2013 day1',null),
-		('Senior 2012 Test Iniziale',null);";
-	$db->query($query) or die($db->error);
-	$db->close();
+function PopulateContests($db) {
+	$Contests=[	'Senior 2013'=>'1994-04-29',
+				'WinterCamp 2011 Ammissione'=>'1994-04-29',
+				'Preimo 2010 TST giorno 1'=>'2013-12-25',
+				'Preimo 2010 TST giorno 2'=>'2010-08-15',
+				'IMO 2013 day1'=>null,
+				'Senior 2012 Test Iniziale'=> null];
+	foreach($Contests as $name=>$date) {
+		Query($db,QueryInsert('Contests',['name'=>$name,'date'=>$date]));
+	}
 
 	echo "Table Contests Populated.\n";
 }
 
-function PopulatePermissions(){
-	$db=new mysqli (dbServer, dbUser, dbPass);
-	if($db->connect_errno) die ($db->connect_error);
+function PopulatePermissions($db){
+	$Users=ManyResultQuery($db, QuerySelect('Users',null,['id']));
+	$Contests=ManyResultQuery($db, QuerySelect('Contests',null,['id']));
 	
-	$query="SELECT id FROM ".dbName.".Users";
-	$UsersResult=$db->query($query) or die($db->error);
-	$n=0;
-	$Users=[];
-	while($Users[$n]=mysqli_fetch_array($UsersResult)['id'])$n++;
-	
-	$query="SELECT id FROM ".dbName.".Contests";
-	$Contests=$db->query($query) or die($db->error);
-	
-	while($Contest=mysqli_fetch_array($Contests)['id']) {
-		$m=mt_rand(0,$n);
-		for($i=0;$i<$m;$i++) {
-			$query="INSERT INTO ".dbName.".Permissions (UserId,ContestId) VALUES (".$Users[$i].",".$Contest.");";
-			$db->query($query) or die($db->error);
+	foreach($Contests as $Contest){
+		foreach($Users as $User) {
+			if( mt_rand(0,1)==1 and IsAdmin($db,$User['id'])==0 ){
+				Query($db,QueryInsert('Permissions',['UserId'=>$User['id'],'ContestId'=>$Contest['id']]));
+			}
 		}
 	}
-	$db->close();
 
 	echo "Table Permissions Populated.\n";
 }
 
-function PopulateContestants() {
-	$db=new mysqli (dbServer, dbUser, dbPass);
-	if($db->connect_errno) die ($db->connect_error);
+function PopulateContestants($db) {
+	$Contestants=[	'Federico'=>'Glaudo',
+					'Giada'=>'Franz',
+					'Gioacchino'=>'Antonelli',
+					'Luca'=>'Minutillo Menga',
+					'Francesco'=>'Florian',
+					'Claudio'=>'Afeltra',
+					'Paolo'=>'Abiuso',
+					'Leonardo'=>'Fiore',
+					'Lorenzo'=>'Benedini',
+					'Davide'=>'Lofano',
+					'Alice'=>'Cortinovis',
+					'Emanuele'=>'Tron',
+					'Matteo'=>'Becchi',
+					'Filippo'=>'Revello',
+					'Fabio'=>'Ferri',
+					'Luigi'=>'Pagano'];
+	foreach($Contestants as $name=>$surname) {
+		Query($db,QueryInsert('Contestants',['name'=>$name,'surname'=>$surname]));
+	}
 	
-	$query="INSERT INTO ".dbName.".`Contestants` (`name`, `surname`) VALUES
-		('Federico', 'Glaudo'),
-		('Giada', 'Franz'),
-		('Gioacchino', 'Antonelli'),
-		('Luca', 'Minutillo Menga'),
-		('Francesco', 'Florian'),
-		('Claudio', 'Afeltra'),
-		('Paolo', 'Abiuso'),
-		('Leonardo', 'Fiore'),
-		('Lorenzo', 'Benedini'),
-		('Davide', 'Lofano'),
-		('Alice', 'Cortinovis'),
-		('Emanuele', 'Tron'),
-		('Matteo', 'Becchi'),
-		('Filippo', 'Revello'),
-		('Fabio', 'Ferri'),
-		('Luigi', 'Pagano');";
-	$db->query($query) or die($db->error);
-	$db->close();
-
 	echo "Table Contestants Populated.\n";
 }
 
-function PopulateParticipations(){
-	$db=new mysqli (dbServer, dbUser, dbPass);
-	if($db->connect_errno) die ($db->connect_error);
-
-	$db->select_db(dbName) or die($db->error);
-
-	$query="SELECT `id` FROM Contestants";
-	$result=$db->query($query) or die($db->error);
-	$n=0;
-	$Contestants=[];
-	while( $Contestants[$n] = mysqli_fetch_array($result)['id'] ) $n++;
+function PopulateParticipations($db){
+	$Contestants=ManyResultQuery($db,QuerySelect('Contestants',null,['id']));
+	$Contests=ManyResultQuery($db,QuerySelect('Contests',null,['id']));
 	
-	$query="SELECT `id` FROM Contests;";
-	$result=$db->query($query) or die($db->error);
-	while ( $row = mysqli_fetch_array($result) ) {
-		$ContestId=$row['id'];
-		$q=mt_rand(0,$n);
-		//~ echo "$ContestId $q\n";
-		for($i=0;$i<$q;$i++) {
-			if(mt_rand(1,10)>=7) continue;
-			
-			$query="INSERT INTO `Participations` (ContestId,ContestantId) VALUES ($ContestId,$Contestants[$i]);";
-			$db->query($query) or die($db->error);
+	foreach($Contests as $Contest) {
+		foreach($Contestants as $Contestant){
+			if( mt_rand(0,1)==1 ) {
+				Query($db, QueryInsert('Participations',['ContestId'=>$Contest['id'],'ContestantId'=>$Contestant['id']]));
+			}
 		}
 	}
-
+	
 	echo "Table Participations Populated.\n";
 }
 
-function PopulateProblems() {
-	$db=new mysqli (dbServer, dbUser, dbPass);
-	if($db->connect_errno) die ($db->connect_error);
+function PopulateProblems($db) {
+	$Problems=[	['name'=>'1','ContestId'=>1],
+				['name'=>'2','ContestId'=>1],
+				['name'=>'3','ContestId'=>1],
+				['name'=>'4','ContestId'=>1],
+				['name'=>'5','ContestId'=>1],
+				['name'=>'6','ContestId'=>1],
+				['name'=>'7','ContestId'=>1],
+				['name'=>'8','ContestId'=>1],
+				['name'=>'9','ContestId'=>1],
+				['name'=>'10','ContestId'=>1],
+				['name'=>'11','ContestId'=>1],
+				['name'=>'12','ContestId'=>1],
+				['name'=>'13','ContestId'=>1],
+				['name'=>'14','ContestId'=>1],
+				['name'=>'15','ContestId'=>1],
+				['name'=>'16','ContestId'=>1],
+				['name'=>'A1','ContestId'=>2],
+				['name'=>'A2','ContestId'=>2],
+				['name'=>'A3','ContestId'=>2],
+				['name'=>'C1','ContestId'=>2],
+				['name'=>'C2','ContestId'=>2],
+				['name'=>'C3','ContestId'=>2],
+				['name'=>'G1','ContestId'=>2],
+				['name'=>'G2','ContestId'=>2],
+				['name'=>'G3','ContestId'=>2],
+				['name'=>'N1','ContestId'=>2],
+				['name'=>'N2','ContestId'=>2],
+				['name'=>'N3','ContestId'=>2],
+				['name'=>'1','ContestId'=>3],
+				['name'=>'2','ContestId'=>3],
+				['name'=>'3','ContestId'=>3],
+				['name'=>'1','ContestId'=>4],
+				['name'=>'2','ContestId'=>4],
+				['name'=>'3','ContestId'=>4],
+				['name'=>'Problem 1','ContestId'=>5],
+				['name'=>'Problem 2','ContestId'=>5],
+				['name'=>'Problem 3','ContestId'=>5]];
 	
-	$query="INSERT INTO ".dbName.".`Problems` (`name`,`ContestId`) VALUES
-		('1',1),
-		('2',1),
-		('3',1),
-		('4',1),
-		('5',1),
-		('6',1),
-		('7',1),
-		('8',1),
-		('9',1),
-		('10',1),
-		('11',1),
-		('12',1),
-		('13',1),
-		('14',1),
-		('15',1),
-		('16',1),
-		('A1',2),
-		('A2',2),
-		('A3',2),
-		('C1',2),
-		('C2',2),
-		('C3',2),
-		('G1',2),
-		('G2',2),
-		('G3',2),
-		('N1',2),
-		('N2',2),
-		('N3',2),
-		('1',3),
-		('2',3),
-		('3',3),
-		('1',4),
-		('2',4),
-		('3',4),
-		('Problem 1',5),
-		('Problem 2',5),
-		('Problem 3',5);";
-	$db->query($query) or die($db->error);
-	$db->close();
+	foreach($Problems as $Problem) {
+		Query($db,QueryInsert('Problems',['name'=>$Problem['name'], 'ContestId'=>$Problem['ContestId']]));
+	}
 
 	echo "Table Problems Populated.\n";
 }
 
-function PopulateCorrections(){
-	$db=new mysqli (dbServer, dbUser, dbPass);
-	if($db->connect_errno) die ($db->connect_error);
-	
-	$db->select_db(dbName) or die($db->error);
-
-	$query="SELECT `id` FROM Users;";
-	$result=$db->query($query) or die($db->error);
-	$qU=0;
-	$Users=[];
-	while($Users[$qU]=mysqli_fetch_array($result)['id'])$qU++;
+function PopulateCorrections($db){
+	$Users=ManyResultQuery($db, QuerySelect('Users',null,['id']));
+	$UsersNumber=count($Users);
+	$Contests=ManyResultQuery($db, QuerySelect('Contests',null,['id']));
 	
 	$CharactersList='abcdefghilmnopqrstuvzabcdefghilmnopqrstuvz        1234567890';
 	$CharactersNumber=strlen($CharactersList);
 	
-	$query="SELECT `id` FROM Contests;";
-	$result=$db->query($query) or die($db->error);
-	while ( $row = mysqli_fetch_array($result) ) {
-		$ContestId=$row['id'];
-
-		$query="SELECT `id` FROM Problems WHERE `ContestId`=$ContestId";
-		$Problems=$db->query($query) or die($db->error);
-
-		$query="SELECT `ContestantId` FROM Participations WHERE `ContestId`=$ContestId";
-		$ContestantsResult=$db->query($query) or die($db->error);
-
-		$n=0;
-		$Contestants=[];
-		while($Contestants[$n]=mysqli_fetch_array($ContestantsResult)['ContestantId'])$n++;
-		
-		while($Prow = mysqli_fetch_array($Problems)){
-			$ProblemId=$Prow['id'];
-			$q=mt_rand(0,$n);
-			//~ echo "$ProblemId $q\n";
-			for($i=0;$i<$q;$i++) {
-				if(mt_rand(1,10)>=7) continue;
-				
-				$CommentLength=mt_rand(0,200);
-				$Comment='';
-				for($j=0;$j<$CommentLength;$j++) $Comment .= $CharactersList[mt_rand(0,$CharactersNumber)];
-				
-				$query="INSERT INTO `Corrections` (ProblemId,ContestantId,mark,comment,UserId) VALUES
-						($ProblemId,$Contestants[$i],".mt_rand(0,7).",'".$Comment."',".$Users[mt_rand(0,$qU-1)].");";
-				$db->query($query) or die($db->error);
+	foreach($Contests as $Contest) {
+		$Problems=ManyResultQuery($db,QuerySelect('Problems',['ContestId'=>$Contest['id']],['id']));
+		$Contestants=ManyResultQuery($db,QuerySelect('Participations',['ContestId'=>$Contest['id']],['ContestantId']));
+		foreach($Problems as $Problem) {
+			foreach($Contestants as $Contestant) {
+				if( mt_rand(0,1)==1 ) {
+					$CommentLength=mt_rand(0,200);
+					$Comment='';
+					for($j=0;$j<$CommentLength;$j++) $Comment .= $CharactersList[mt_rand(0,$CharactersNumber)];
+					$UserId=$Users[mt_rand(0,$UsersNumber-1)]['id'];
+					Query($db,QueryInsert('Corrections', 
+					['ProblemId'=>$Problem['id'], 'ContestantId'=>$Contestant['ContestantId'], 'mark'=>mt_rand(0,7), 'comment'=>$Comment, 'UserId'=>$UserId]
+					));
+				}
 			}
 		}
-		
 	}
 
 	echo "Table Corrections Populated.\n";
@@ -380,14 +306,18 @@ function PopulateCorrections(){
 
 CreateDatabase();
 
-PopulateUsers();
-PopulateAdministrators();
-PopulateContests();
-PopulatePermissions();
-PopulateContestants();
-PopulateParticipations();
-PopulateProblems();
-PopulateCorrections();
+$db=OpenDbConnection();
+
+PopulateUsers($db);
+PopulateAdministrators($db);
+PopulateContests($db);
+PopulatePermissions($db);
+PopulateContestants($db);
+PopulateParticipations($db);
+PopulateProblems($db);
+PopulateCorrections($db);
+
+$db->close();
 
 echo "THE DATABASE IS COMPLETELY INITIALIZED!\n";
 
