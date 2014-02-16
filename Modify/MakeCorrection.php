1d240e12e7b375450c0fa03a9b9f769e693a4650
Federico Glaudo <dario2994@gmail.com>
2014-02-16 15:39:21 +0100

    Riscritto decentemente MakeCorrection.php

--- Modify/MakeCorrection.php
+++ Modify/MakeCorrection.php
@@ -4,6 +4,45 @@ SuperRequire_once('General','SessionManager.php');
 SuperRequire_once('General','sqlUtilities.php');
 SuperRequire_once('General','PermissionManager.php');
 
+function MakeCorrection($db, $ContestId, $ContestantId, $mark, $comment) {
+	if( is_null( $ContestId ) ) {
+		return ['type'=>'bad', 'text'=>'La gara scelta non esiste'];
+	}
+
+	$Blocked=OneResultQuery($db, QuerySelect('Contests',['id'=>$ContestId],['blocked']))['blocked'];
+	if( $Blocked==1 ) {
+		return ['type'=>'bad', 'text'=>'Le correzioni della gara scelta sono terminate'];
+	}
+
+	$Permission=VerifyPermission( $db, GetUserIdBySession() , $ContestId );
+	if( $Permission==0 ) {
+		return ['type'=>'bad', 'text'=>'Non hai i permessi per correggere questa gara'] ;
+	}
+
+	if( is_null( $mark ) ) { //TODO: Controllare che sia un voto numerico o quasi
+		return ['type'=>'bad', 'text'=>'Devi scegliere un voto prima di salvare la correzione'];
+	}
+
+	$ParticipationId= OneResultQuery ($db, QuerySelect('Participations', ['ContestId'=>$ContestId, 'ContestantId'=>$ContestantId]) ) ;
+	if( is_null ( $ParticipationId ) ) {
+		return ['type'=>'bad', 'text'=>'Il partecipante selezionato non ha partecipato alla gara'] ;
+	}
+
+	$Correction =OneResultQuery ( $db,QuerySelect('Corrections', ['ProblemId'=>$ProblemId, 'ContestantId'=>$ContestantId]) );
+
+	if( is_null($Correction) ) {
+		Query( $db,QueryInsert('Corrections', 
+		['ProblemId'=>$ProblemId, 'ContestantId'=>$ContestantId, 'mark'=>$mark, 'comment'=>$comment, 'UserId'=>GetUserIdBySession() ]) );
+	}
+
+	else {
+		if( $mark == $Correction['mark'] ) Query( $db,QueryUpdate('Corrections', ['id'=>$Correction['id']] , ['comment'=>$comment]) );
+		else Query( $db,QueryUpdate('Corrections', ['id'=>$Correction['id']] , ['mark'=>$mark, 'comment'=>$comment, 'UserId'=>GetUserIdBySession() ]) );
+	}
+	
+	return ['type'=>'good', 'text'=>'Correzione salvata con successo'];
+}
+
 $data=json_decode( $_POST['data'] , 1);
 $ContestantId=$data['ContestantId'];
 $ProblemId=$data['ProblemId'];
@@ -13,44 +52,7 @@ $comment=$data['comment'];
 $db=OpenDbConnection();
 $ContestId=OneResultQuery($db, QuerySelect( 'Problems', ['id'=>$ProblemId], ['ContestId'] ))['ContestId'];
 
-if( is_null( $ContestId ) ) {
-	$db->close();
-	echo json_encode( ['type'=>'bad', 'text'=>'La gara scelta non esiste'] );
-	die();
-}
-
-$Permission=VerifyPermission( $db, GetUserIdBySession() , $ContestId );
-
-if( $Permission==0 ) {
-	$db->close();
-	echo json_encode( ['type'=>'bad', 'text'=>'Non hai i permessi per correggere questa gara'] );
-	die();
-}
-
-if( is_null( $mark ) ) { //TODO: Controllare che sia un voto numerico o quasi
-	$db->close();
-	echo json_encode( ['type'=>'bad', 'text'=>'Devi scegliere un voto prima di salvare la correzione'] );
-	die();
-}
-
-$ParticipationId= OneResultQuery ($db, QuerySelect('Participations', ['ContestId'=>$ContestId, 'ContestantId'=>$ContestantId]) ) ;
-if( is_null ( $ParticipationId ) ) {
-	$db->close();
-	echo json_encode( ['type'=>'bad', 'text'=>'Il partecipante selezionato non ha partecipato alla gara'] );
-}
-
-$Correction =OneResultQuery ( $db,QuerySelect('Corrections', ['ProblemId'=>$ProblemId, 'ContestantId'=>$ContestantId]) );
-
-if( is_null($Correction) ) {
-	Query( $db,QueryInsert('Corrections', 
-	['ProblemId'=>$ProblemId, 'ContestantId'=>$ContestantId, 'mark'=>$mark, 'comment'=>$comment, 'UserId'=>GetUserIdBySession() ]) );
-}
-
-else {
-	if( $mark == $Correction['mark'] ) Query( $db,QueryUpdate('Corrections', ['id'=>$Correction['id']] , ['comment'=>$comment]) );
-	else Query( $db,QueryUpdate('Corrections', ['id'=>$Correction['id']] , ['mark'=>$mark, 'comment'=>$comment, 'UserId'=>GetUserIdBySession() ]) );
-}
+echo json_encode( MakeCorrection($db, $ContestId, $ContestantId, $mark, $comment) );
 
 $db->close();
-echo json_encode( ['type'=>'good', 'text'=>'Correzione salvata con successo'] );
 ?>
