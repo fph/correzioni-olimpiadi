2c5493d4c3939f7c62d5e8e676481be8f6de3125
Giada Franz <walypala23@gmail.com>
2014-02-17 10:37:19 +0100

    Finita creazione dei partecipanti, anche con il setTimeout (a meno del colore che è da cambiare)

--- View/js/global.js
+++ View/js/global.js
@@ -11,3 +11,38 @@ function Redirect(pageUrl, getElements) {
 	}
 	document.location=url;
 }
+
+function AddRow(columns, classes, RedirectUrl, getElements, orderBy){
+	var InformationTable=document.getElementsByClassName('InformationTable')[0];
+	var tbodyEl=InformationTable.getElementsByTagName('tbody')[0];
+	
+	var newRow=document.createElement('tr');
+	
+	newRow.className='new_row';
+	for (var cl in classes) newRow.className+=' '+classes[cl];
+	setTimeout(function(){ newRow.classList.remove('new_row'); },5000);
+	
+	newRow.onclick=function(){Redirect(RedirectUrl, getElements)};
+	
+	var newRowHTML='';
+	for (var co in columns) newRowHTML+="<td class='"+co+"Column'>"+columns[co]+"</td>";
+	newRow.innerHTML=newRowHTML;
+	
+	var childs=tbodyEl.getElementsByTagName('tr');
+	
+	var compareEl;
+	for (var co in columns) if (co==orderBy) compareEl=columns[co];
+	
+	var aggiunto=false;
+	for (var i=0; i<childs.length; i++) {
+		var sortingColumn=childs[i].getElementsByClassName(orderBy+'Column')[0];
+		if (String(compareEl).localeCompare(String(sortingColumn.innerHTML))<0) {
+			tbodyEl.insertBefore(newRow,childs[i]);
+			aggiunto=true;
+			break;
+		}
+	}
+	if (aggiunto==false) tbodyEl.appendChild(newRow);
+	
+	for (var co in columns) document.getElementById(co+'_input').value='';
+}
