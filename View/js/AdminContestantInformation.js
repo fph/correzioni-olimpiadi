64a4d1061eab9cd46d3312da8ef4132598db88d1
Federico Glaudo <dario2994@gmail.com>
2014-02-19 22:50:02 +0100

    Iniziato la modifica del titolo

--- View/js/AdminContestantInformation.js
+++ View/js/AdminContestantInformation.js
@@ -24,14 +24,31 @@ function StandardButtons() {
 
 function ModifyContestantName() {
 	ModificationButtons();
-	document.getElementById('ContestantName').setAttribute('contenteditable','true');
-	document.getElementById('ContestantSurname').setAttribute('contenteditable','true');
+	var surname=document.getElementById('ContestantSurname');
+	var name=document.getElementById('ContestantName');
+	surname.setAttribute('contenteditable','true');
+	name.setAttribute('contenteditable','true');
+	surname.dataset.OldValue=surname.innerHTML;
+	name.dataset.OldValue=name.innerHTML;
 }
 
 function SendModification(ContestantId){
 	StandardButtons();
+	var surname=document.getElementById('ContestantSurname');
+	var name=document.getElementById('ContestantName');
+	surname.setAttribute('contenteditable','false');
+	name.setAttribute('contenteditable','false');
+	// Mandare la richiesta al server!
 }
 
 function CancelModification(ContestantId){
 	StandardButtons();
+	var surname=document.getElementById('ContestantSurname');
+	var name=document.getElementById('ContestantName');
+	surname.setAttribute('contenteditable','false');
+	name.setAttribute('contenteditable','false');
+	surname.innerHTML=surname.dataset.OldValue;
+	name.innerHTML=name.dataset.OldValue;
+	surname.dataset.OldValue=null;
+	name.dataset.OldValue=null;
 }
