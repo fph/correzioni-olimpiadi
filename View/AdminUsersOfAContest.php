fa1df33ae3eb717025f8514c1ef30991cecd3e17
Federico Glaudo <dario2994@gmail.com>
2014-02-24 23:57:10 +0100

    Aggiunto la classe dei tr degli admin

--- View/AdminUsersOfAContest.php
+++ View/AdminUsersOfAContest.php
@@ -28,6 +28,7 @@ foreach($v_users as $user) {
 	$row=[
 	'values'=>['username'=>$user['username'] ],
 	'data'=>['user_id'=>$user['id']] ];
+	if( $user['admin'] ) $row['class']=['NoTrashButton'];
 	$rows[]=$row;
 }
 
