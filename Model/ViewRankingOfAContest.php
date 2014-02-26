3360992ba7fb2b837664eb33b1e1a7e9da5708bf
Federico Glaudo <dario2994@gmail.com>
2014-02-26 01:19:27 +0100

    Scritto E AGGIUNTO  ViewRankingContest

---
+++ Model/ViewRankingOfAContest.php
@@ -0,0 +1,41 @@
+<?php
+	//~ $start_time = microtime(true);
+	
+	require_once "../Utilities.php";
+	SuperRequire_once("General","sqlUtilities.php");
+	SuperRequire_once("General", "TemplateCreation.php");
+	SuperRequire_once("General", "PermissionManager.php");
+	
+	$db=OpenDbConnection();
+	
+	$ContestId=$_GET['ContestId'];
+	
+	CheckPagePermission($db,$ContestId);
+	
+	//PermissionChecked	
+	
+	$v_contest=OneResultQuery($db, QuerySelect('Contests',['id'=>$ContestId]));
+	$v_problems=ManyResultQuery($db, QuerySelect('Problems',['ContestId'=>$ContestId]));
+	$participations=ManyResultQuery($db, QuerySelect('Participations',['ContestId'=>$ContestId],['ContestantId']));
+	$v_contestants=[];
+	foreach($participations as $X) {
+		$contestant=OneResultQuery($db, QuerySelect('Contestants',['id'=>$X['ContestantId']]));
+		$contestant['marks']=[];
+		$v_contestants[$X['ContestantId']]=$contestant;
+	}
+	foreach($v_problems as $problem) {
+		$ProblemCorrections=ManyResultQuery($db, QuerySelect('Corrections',['ProblemId'=>$problem['id']]));
+		foreach($ProblemCorrections as $correction) {
+			$v_contestants[$correction['ContestantId']]['marks'][$correction['ProblemId']]=$correction['mark'];
+		}
+	}
+	
+	
+	$db->close();
+	
+	TemplatePage('ViewRankingOfAContest', [	'Index'=>'index.php',
+											'Gare'=>'ViewContests.php',
+											$v_contest['name']=>'ViewContestInformation.php?ContestId='.$ContestId,
+											'Classifica'=>'ViewRankingOfAContest.php?ContestId='.$ContestId]);
+	//~ echo(number_format(microtime(true) - $start_time, 5));
+?>
---
+++ View/ViewRankingOfAContest.php
@@ -0,0 +1,39 @@
+<?php
+global $v_contest, $v_problems, $v_contestants;
+?>
+
+<h2 class='PageTitle'>
+	Classifica
+</h2>
+<h3 class='PageSubtitle'>
+	<?=$v_contest['name']?>
+</h3>
+
+<?php
+	$columns=[];
+	$columns[]=['id'=>'contestant', 'name'=>'Partecipante', 'class'=>'SurnameAndNameColumn', 'order'=>1];
+	
+	usort($v_problems, build_sorter('name'));
+	foreach($v_problems as $problem) {
+		$columns[]=['id'=>strval( $problem['id']), 'name'=>$problem['name'], 'class'=>'ProblemColumn', 'order'=>1];
+	}
+	$columns[]=['id'=>'score', 'name'=>'Punteggio', 'class'=>'ScoreColumn', 'order'=>1]; 
+	
+	$rows=[];
+	foreach( $v_contestants as $contestant ) {
+		$values=['contestant'=>$contestant['surname'].' '.$contestant['name']];
+		$total=0;
+		foreach( $v_problems as $problem ) {
+			if( !is_null( $contestant['marks'][$problem['id']] ) ) {
+				$values[$problem['id']]=$contestant['marks'][$problem['id']];
+				$total+=intval($contestant['marks'][$problem['id']]);
+			}
+		}
+		$values['score']=$total;
+		$rows[]=['values'=>$values];
+	}
+	
+	$table=['columns'=>$columns, 'rows'=>$rows, 'id'=> 'ContestRankingTable', 'InitialOrder'=>'score'];
+	InsertTable($table);
+	
+?>
