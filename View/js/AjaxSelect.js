5c8ab3f24ae6e43f6f3f5f39590ca786c88b9f49
Federico Glaudo <dario2994@gmail.com>
2014-03-02 00:46:48 +0100

    Migliorato ancora AjaxSelect con aggiunta di eventi

--- View/js/AjaxSelect.js
+++ View/js/AjaxSelect.js
@@ -26,20 +26,20 @@ function ListOptions( obj ) {
 			var text=GetDataAttribute(this,'InputText');
 			container.getElementsByClassName('AjaxSelectValue')[0].value=value;
 			container.getElementsByClassName('AjaxSelectText')[0].value=text;
-			CleanOptions( container.id );
+			OptionsDiv.classList.add('hidden');
 		});
 		
 		OptionsDiv.appendChild( OptionDiv );
 	}
 }
 
-function OnKeyUpOrFocus(e){
-	var id=this.parentNode.id;
-	var val=this.value;
-	var type=GetDataAttribute(this,'request_type');
-	var LastString=GetDataAttribute(this,'last_string');
+function GetSelectSuggestions( input ){
+	var id=input.parentNode.id;
+	var val=input.value;
+	var type=GetDataAttribute(input,'request_type');
+	var LastString=GetDataAttribute(input,'last_string');
 	if( LastString == val ) return;
-	else SetDataAttribute(this,'last_string',val);
+	else SetDataAttribute(input,'last_string',val);
 	
 	if( val == '' ) CleanOptions(id);
 	else MakeAjaxRequest('../Modify/SelectSuggestions.php',{id: id, str:val,type: type},ListOptions,1);
@@ -50,19 +50,33 @@ function RenderSelect( id , type ) {
 	TextSelect.classList.add('AjaxSelectText');
 	SetDataAttribute(TextSelect,'request_type',type);
 	SetDataAttribute(TextSelect,'last_string','');
-	TextSelect.addEventListener('keyup', OnKeyUpOrFocus);
-	TextSelect.addEventListener('focus', OnKeyUpOrFocus);
+	TextSelect.addEventListener('keyup', function(e){
+		GetSelectSuggestions(this);
+		var ValueInput=this.nextSibling;
+		ValueInput.value='';
+		this.classList.remove('AjaxSelectOptionGood');
+		this.classList.remove('AjaxSelectOptionBad');
+	});
 	
 	TextSelect.addEventListener('blur',function(e) {
-		var el=this.parentNode.getElementsByClassName('AjaxSelectOptionsDiv')[0];
+		var input=this;
+		
+		//This setTimeout is used to let click event fire and be executed, and only then to execute blur (otherwise everything gets bugged)
 		setTimeout(function(){
+			var el=input.parentNode.getElementsByClassName('AjaxSelectOptionsDiv')[0];
 			el.classList.add('hidden');
+			var ValueInput=input.nextSibling;
+			if( ValueInput.value != '' ) input.classList.add('AjaxSelectOptionGood');
+			else if( input.value != '' ) input.classList.add('AjaxSelectOptionBad'); 
 		},100);
 	});
 	
 	TextSelect.addEventListener('focus',function(e) {
-		var el=this.parentNode.getElementsByClassName('AjaxSelectOptionsDiv')[0];
-		el.classList.remove('hidden');
+		GetSelectSuggestions(this);
+		var OptionsDiv=this.parentNode.getElementsByClassName('AjaxSelectOptionsDiv')[0];
+		OptionsDiv.classList.remove('hidden');
+		this.classList.remove('AjaxSelectOptionGood');
+		this.classList.remove('AjaxSelectOptionBad');
 	});
 	
 	var ValueSelect= document.createElement('input');
