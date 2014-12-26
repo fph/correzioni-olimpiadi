ed45c78ec8a9e9d81c5d832a56a522eb4fe5beb1
Federico Glaudo <dario2994@gmail.com>
2014-12-26 17:12:47 +0100

    Aggiunto LinkTable.js e aggiornato la documentazione

---
+++ View/js/LinkTable.js
@@ -0,0 +1,34 @@
+function RenderLinkTable( obj ) {
+	var table=document.createElement('table');
+	table.classList.add('LinkTable');
+	var tbody=document.createElement('tbody');
+	for(var i=0;i<obj.length;i++) {
+		var tr=document.createElement('tr');
+		var td=document.createElement('td');
+		var span=document.createElement('span');
+		span.innerHTML=obj[i].name;
+		SetDataAttribute( span, 'redirect', JSON.stringify(obj[i].redirect) );
+		span.classList.add('CursorPointer');
+		
+		
+		span.addEventListener( 'click', function(e) {
+			var DataRedirect=JSON.parse( GetDataAttribute(this,'redirect') );
+			Redirect(DataRedirect.url,DataRedirect.parameters);
+		} );
+		
+		td.appendChild(span);
+		tr.appendChild(td);
+		tbody.appendChild(tr);
+	}
+	table.appendChild(tbody);
+	return table;
+}
+
+//The variable LinkTablesInformation is defined server-side
+if( LinkTablesInformation != null ){
+	for(var i=0;i<LinkTablesInformation.length;i++) {
+		var LinkTable=LinkTablesInformation[i];
+		var DivToLinkTable=document.getElementById('DivToLinkTable'+i);
+		DivToLinkTable.parentNode.replaceChild( RenderLinkTable(LinkTable), DivToLinkTable);
+	}
+}
