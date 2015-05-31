9165e3df78c5130af961ef047615bc9f78bd0cde
Federico Glaudo <dario2994@gmail.com>
2015-05-31 15:58:33 +0200

    fix

--- View/js/AjaxManager.js
+++ View/js/AjaxManager.js
@@ -1,6 +1,6 @@
 function MakeAjaxRequest(url, data, exec, NoMessage) {
 	var ajaxReq;
-	if (window.XMLHttpRequest)ajaxReq = new XMLHttpRequest();
+	if (window.XMLHttpRequest) ajaxReq = new XMLHttpRequest();
 	else ajaxReq = new ActiveXObject('Microsoft.XMLHTTP');
 	ajaxReq.open('POST', url, true);
 	ajaxReq.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
@@ -10,7 +10,7 @@ function MakeAjaxRequest(url, data, exec, NoMessage) {
 		if (ajaxReq.readyState == 4 && ajaxReq.status == 200) {
 			document.getElementsByTagName('html')[0].classList.remove('CursorWait');
 			var response = JSON.parse(ajaxReq.responseText); 
-			if (NoMessage == null || NoMessage != 1)ShowMessage(response.type, response.text);
+			if (NoMessage == null || NoMessage != 1) ShowMessage(response.type, response.text);
 			if (exec) exec(response);
 		}
 	};
