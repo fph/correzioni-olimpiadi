7d8074b430dfc3c7f5195d76c96e61cb1de55970
Federico Poloni <federico.poloni@unipi.it>
2025-01-18 09:56:44 +0100

    Removed unused TableInformation

--- sqlUtilities.php
+++ sqlUtilities.php
@@ -1,15 +1,5 @@
 <?php
 
-$TableInformation = array(
-	'Contestants' => array('id', 'name', 'surname', 'school', 'email', 'LastOlympicYear'), //LastOlympicYear is the last IMO year for that contestant
-	'Contests' => array('id', 'name', 'date', 'NotAcceptedEmail', 'ForwardRegistrationEmail'),
-	'Corrections' => array('id', 'ProblemId', 'ContestantId', 'mark', 'comment', 'UserId'),
-	'Participations' => array('id', 'ContestId', 'ContestantId', 'email', 'solutions', 'VolunteerRequest'),
-	'Permissions' => array('id', 'UserId', 'ContestId'),
-	'Problems' => array('id', 'ContestId', 'name'),
-	'Users' => array('id', 'username', 'passHash', 'role'),
-);
-
 function EscapeInput($value) {
 	if (is_null($value)) return 'NULL';
 	if (!is_string($value) and !is_int($value) and !is_float($value)) {
